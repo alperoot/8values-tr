ae94a4190dafd43dcf70c54177f4ec8f8e5cdcbb
alperoot <alperenmirac@gmail.com>
2018-10-24 13:19:33 +0300

    Fiddlesticks! What now.

---
+++ ideologies.js
@@ -0,0 +1,470 @@
+ideologies = [
+    {
+        "name": "Anarko-Komünizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Lİbertanyan Komünizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Troçkizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Marksizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 80
+        }
+    },
+    {
+        "name": "De Leonizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Leninizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Stalinizm/Maoizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Dini Komünizm",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Devletçi Komünizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Teokratik Sosyalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Dini Sosyalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 70,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Demokratik Sosyalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Devrimci Sosyalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Libertaryan Sosyalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Anarko-Sendikalizm",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Solcu Popülizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Teokratik Distribütizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Distribütizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Sosyal Liberalizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Hristiyan Demokrasisi",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 40,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Sosyal Demokrasi",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Gelişimci",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Anarko-Mutualizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Ulusal Totaliterlik",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Küresel Totaliterlik",
+        "stats": {
+            "econ": 50,
+            "dipl": 80,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Teknokrasi",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Merkeziyetçilik",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Liberalizm",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Dini Anarşizm",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Sağcı Halkçılık",
+        "stats": {
+            "econ": 40,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Orta Muhafazakarlık",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Gericilik",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Sosyal Libertaryanizm",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Libertaryanizm",
+        "stats": {
+            "econ": 40,
+            "dipl": 60,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Anarko-Egoizm",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Nazizm",
+        "stats": {
+            "econ": 40,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Otokrasi",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Faşizm",
+        "stats": {
+            "econ": 40,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Kapitalist Faşizm",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Muhafazakarlık",
+        "stats": {
+            "econ": 30,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Neo-Liberalizm",
+        "stats": {
+            "econ": 30,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Klasik Liberalizm",
+        "stats": {
+            "econ": 30,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Otoriter Kapitalizm",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 20,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Devletçi Kapitalizm",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Neo-Muhafazakarlık",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Aşırı Muhafazakarlık",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Libertaryan Kapitalizm",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+        "name": "Piyasa Anarşizmi",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Objectivizm",
+        "stats": {
+            "econ": 10,
+            "dipl": 50,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Totaliter Kapitalizm",
+        "stats": {
+            "econ": 0,
+            "dipl": 30,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Ultra-Kapitalizm",
+        "stats": {
+            "econ": 0,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Anarko-Kapitalizm",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,632 @@
+questions = [
+    {
+        "question": "Şirketler tarafından baskı, devlet baskısından daha büyük bir sorundur.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Devletin tüketiciyi koruması için ekonomide araya girmesi gereklidir.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Piyasalar ne kadar özgür ise millet de o kadar özgürdür.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Dengeli bir bütçe tutmak, tüm vatandaşların ekonomik durumunu iyi tutmaktan daha önemlidir.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "İşi piyasaylaa bırakmak yerine toplum tarafından desteklenen araştırmalar yürütmek daha verimlidir.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Uluslararası ticaret ülke için yararlıdır.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "İnsanlara yeteneklerine göre davranmak yerine gereksinimlerine göre davranmak daha faydalıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Bütün sosyal yardım programlarının yerine yalnızca bir tane özel dernek getirilebilir.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Fakir insanlara yardım etmek için zenginlerin vergileri artırılmalıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Miras, gerçek bir zenginlik faktörü olarak kabul edilmelidir.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Elektrik ve yollar gibi halk tarafından kullanılan hizmetlerin sahibi de halk olmalıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Devletin ekonomiye fazla karışması ekonomiye karşı bir tehdittir.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Daha fazla ödeme gücü olanlar daha iyi sağlık hizmetleri görmelilerdir.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Kaliteli eğitim herkesin hakkıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "İşçiler tarafından kullanılan aletlerin sahibi yine işçiler olmalıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Birleşmiş Milletler dağıtılmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Devletimiz tarafından yapılan askeri müdahaleler çoğunlukla bizim güvenliğimiz içindir.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Avrupa Biriği gibi bölgesel birlikler desteklenmelidir.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Milli egemenliğimizi sağlamak çok önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Tüm dünyanın tek bir hükumet tarafından yönetilmesi insanlık için yararlıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Daha fazla güçlenmektense barış içinde olmak daha önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Ülkemiz, başka bir ülkeye karşı açtığı bir savaşın sebebini açıklamak zorunda değildir.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Orduya harcanan para boşa harcanan paradır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Uluslararası yapılan yardımlar para kaybıdır.",
+        "effect": {
+            "econ": -5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Benim milliyetim en iyisidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Araştırmalar uluslararası ölçütlerde gerçekleştirilmelidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Hükumetler yurtdışındaki topluluklarından da sorumlu olmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Otoriter bir devleti protesto ederken bile şiddet kullanılmamalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Dini değerlerim olabildiğince yayılmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Milli değerlerim olabildiğince yayılmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Yasa ve düzeni sağlamak oldukça önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Genel toplum yanlış kararlar almaya yatkındır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Kurbanı olmayan suçlar (örneğin uyuşturucu kullanımı) suç sayılmamalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Terörden korunmak için bazı kişisel özgürlüklerden vazgeçilmesi bir zorunluluktur.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Devlet takibi gereklidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eyalet siteminin kullanılması özgürlüğe karşıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Bir insan, her ne olursa olsun kendi ülkesinin yanında durmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Hiçbir otorite sorgulanmadan bırakılmamalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Hiyerarşik (katmanlı) devlet tipi en iyi devlet tipidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Genel halkın kararı yanlış olsa bile hükumetin bu kararı uygulaması gerekmektedir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Bir devlet ancak lideri kadar güçlüdür.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Demokrasi, bir karar verme sürecinden çok daha fazlasıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Çevrenin korunması için getirilen kısıtlamalar olmazsa olmazdır.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Daha iyi günleri bilim ve teknoloji getirecektir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Çocuklara din ya da gelenek eğitimi verilmelidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Kendi başına gelenekler hiçbir anlam ifade etmez.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Dinin hükumet üstünde bir rolü olmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Diğer kurumlar nasıl vergilenidiriliyorsa camiler ve kiliseler de aynı şekilde vergilendirilmelidir.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Şu an hayatımızdaki en büyük tehditlerden birisi küresel ısınmadır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "İklim değişimiyle savaşmak için tüm dünya olarak birleşmek çok önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Eskiden insanlık, şimdiki halinden çok daha iyiydi.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Geleneklerimizi devam ettirmemiz çok önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Uzun vadede, kendi ömrümüzden ötesini de düşünmek gerekir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Entelektüel değerler kültürel değerlerden daha önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Uyuşturucu kullanımı yasadışı olmaktan çıkarılmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "Eşcinsel evliliği yasadışı olmaktan çıkarılmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Hiçbir kültür diğerlerinden üstün değildir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Evlilik dışı cinsel ilişki ahlaki değildir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Ülkeye kabul edilen göçmenler asimile edilmelidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Kürtaj çoğu durumda yasak olmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Geçerli bir sebebi olmayan insanlara silah ruhsatı verilmemelidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Ödemeini tek bir kaynaktan alan, evrensel bir sağlık hizmeti olmalıdır.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Fahişelik yasadışı olmalıdır.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Aile değerlerini korumak çok önemlidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Hiçbir etkeni göz önüne almadan topluluk olarak gelişmeye çalışmak tehlikelidir.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Genetik değişim, insan üzerinde olsa bile iyi sonuçlara yol açar.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Sınırlarımızı göçmenlere açmalıyız.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Hükumet sınırları içinde bulunan vatandaşlarını önemediği kadar yabancı vatandaşlarını da önemsemelidir.",
+        "effect": {
+            "econ": 10,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Kültür veya cinsel tercihine bakmaksızın herkes eşit değerlendirilmelidir.",
+        "effect": {
+            "econ": 10,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Benim grubumun amacına ulaşması diğerlerinden daha önemlidir.",
+        "effect": {
+            "econ": -10,
+            "dipl": -10,
+            "govt": -10,
+            "scty": -10
+        }
+    }
+];
